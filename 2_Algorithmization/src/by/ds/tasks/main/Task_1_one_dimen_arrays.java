0103abb71488bbda708ab31f6e0d95efd866cc2d
Dima Shimanel <en_di@mail.ru>
2019-11-30 17:55:20 +0200

    A[N]

--- 2_Algorithmization/src/by/ds/tasks/main/Task_1_one_dimen_arrays.java
+++ 2_Algorithmization/src/by/ds/tasks/main/Task_1_one_dimen_arrays.java
@@ -1,5 +1,30 @@
 package by.ds.tasks.main;
+import java.util.Scanner;
+
+/*
+ * 1. В массив A[N] занесены натуральные числа.
+ *    Найти сумму тех элементов, которые кратны данному К.
+ */
 
 public class Task_1_one_dimen_arrays {
-
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter the size of the array: ");
+		int n = in.nextInt();
+		int[] A = new int[n];
+		System.out.print("Enter K: ");
+		int k = in.nextInt();
+		int sum = 0;
+		
+		for(int i = 0; i < n; i++) {
+			A[i] = (int)(Math.random() * 100);
+			System.out.print(A[i] + " ");
+			if (A[i] % k == 0)
+				sum += A[i];
+		}
+		System.out.println(" ");
+		System.out.print("Sum is: " + sum);
+		in.close();
+	}
 }
