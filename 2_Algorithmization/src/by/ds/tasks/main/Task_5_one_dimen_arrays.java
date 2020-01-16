3728c8bf11cdb31759d8d2c9d937cb88c5d00ef8
Dima Shimanel <en_di@mail.ru>
2020-01-16 12:26:24 +0200

    ai > i

---
+++ 2_Algorithmization/src/by/ds/tasks/main/Task_5_one_dimen_arrays.java
@@ -0,0 +1,32 @@
+package by.ds.tasks.main;
+
+/*
+ * 5. Даны целые числа а1, а2,..., аn. Вывести на 
+ * 	  печать только те числа, для которых ai > i.
+ */
+public class Task_5_one_dimen_arrays {
+	public static void main(String[] args) {
+		
+		int [] array1 = {5,2,6,1,4,3,0,-9,10};
+		
+		for(int i = 0; i < array1.length; i++) {
+			System.out.print(" " + i + " ");
+		}
+		System.out.println("");
+		
+		for(int i = 0; i < array1.length; i++) {
+			System.out.print(" "+ array1[i] + " ");
+		}
+		System.out.println("");
+		
+		for (int i = 0; i < array1.length; i++) {
+			if(array1[i] > i) {
+				System.out.print(" " + array1[i] + " ");
+			}else {
+				System.out.print(" - ");
+			}
+			
+		}
+	}
+
+}
