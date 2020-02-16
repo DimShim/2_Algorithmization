db417d914cebb2caf5c0f5dd12f24e67c7d3da93
Dima Shimanel <en_di@mail.ru>
2020-02-16 17:49:40 +0200

    2.1

--- 2_Algorithmization/src/by/ds/tasks/main/Task_11_1_arrays.java
+++ 2_Algorithmization/src/by/ds/tasks/main/Task_11_1_arrays.java
@@ -1,5 +1,31 @@
 package by.ds.tasks.main;
 
+/* 
+ * 11.1 Дана матрица. Вывести на экран все нечетыне
+ * 		столбцы, у которых первый элемент больше последнего.
+ */
 public class Task_11_1_arrays {
-
+	public static void main(String [] args) {
+		int t, i;
+		int table[][] = new int [3][9];
+								//Заполняем матрицу		
+		for(t = 0; t < 3; ++t) {
+			for(i = 0; i < 9; ++i) {
+				table[t][i] = (int) (Math.random()*10);
+				System.out.print(table[t][i] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+								//Находим нужный нам по условию столбец		
+		for(int a = 1; a < 9; a+=2) {
+			if(table[0][a] > table[2][a]) {
+				System.out.println(table[0][a]);
+				System.out.println(table[1][a]);
+				System.out.println(table[2][a]);
+				System.out.println();
+			}	
+		}
+		
+	}
 }
