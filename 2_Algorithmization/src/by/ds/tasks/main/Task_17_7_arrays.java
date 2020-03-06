eb9ac7de3f72918394415c49de1a8175d6075683
Dima Shimanel <en_di@mail.ru>
2020-03-06 18:56:40 +0200

    a[i,j] = sin((i^2-j^2)/n)

---
+++ 2_Algorithmization/src/by/ds/tasks/main/Task_17_7_arrays.java
@@ -0,0 +1,40 @@
+package by.ds.tasks.main;
+import java.util.Scanner;
+
+/*
+ * 	17.7 Сформировать квадратную матрицу порядка 
+ * 		 N  по правилу:
+ * 
+ * 						A[I,J]=sin((I*I - J*J)/N)
+ * 
+ * 		 и подсчитать количество положительных
+ * 		 элементов в ней.
+ */
+
+public class Task_17_7_arrays {
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		System.out.print("Введите размерность матрицы N (четное): ");
+		int n = in.nextInt();
+		System.out.println();
+		
+		double matrix[][] = new double[n][n]; // double из-зa sin
+		int a = 0; // Количество положительных элементов в матрице
+		
+		// Заполняем матрицу согласно правилу
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				matrix[i][j] = Math.sin((double)(i * i - j * j) / n);
+				System.out.printf("%.2f  ", matrix[i][j]);
+				if(matrix[i][j] > 0)
+					a++;
+			}
+			System.out.println();
+		}
+		System.out.println();
+		System.out.print("Количество положительных элементов в матрице: " + a);
+		in.close();
+	}
+
+}
