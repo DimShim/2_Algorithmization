421bd25a7993756e46d83bb393b8be46226b914b
Dima Shimanel <en_di@mail.ru>
2020-03-15 13:00:32 +0200

    15.03.20

--- 2_Algorithmization/src/by/ds/tasks/main/Task_22_12_arrays.java
+++ 2_Algorithmization/src/by/ds/tasks/main/Task_22_12_arrays.java
@@ -18,7 +18,7 @@ public class Task_22_12_arrays {
 		
 		int matrix[][] = new int[a][b];
 		
-		// Заполняем массив
+		// Заполняем массив числами
 		for(int i = 0; i < matrix.length; i++) {
 			for(int j = 0; j < matrix[i].length; j++) {
 				matrix[i][j] = (int)(Math.random()*100);
